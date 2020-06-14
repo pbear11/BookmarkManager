b7e4d06ae733215e4d6e2071e5404000133364b6
pbear11 <bluekarasu11@gmail.com>
2020-06-14 23:11:34 +0900

    initial commit / all main functionalities are done / can create and delete folders/links, can go inside folders and also go back

---
+++ src/popup/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+body {
+  margin: 0 !important;
+}
+.icon {
+  font-size: 12px;
+  cursor: pointer;
+}
+@font-face {
+  font-family: 'icomoon';
+  src:  url('https://i.icomoon.io/public/temp/51ece55b79/UntitledProject/icomoon.eot?xn63nf');
+  src:  url('https://i.icomoon.io/public/temp/51ece55b79/UntitledProject/icomoon.eot?xn63nf#iefix') format('embedded-opentype'),
+    url('https://i.icomoon.io/public/temp/51ece55b79/UntitledProject/icomoon.ttf?xn63nf') format('truetype'),
+    url('https://i.icomoon.io/public/temp/51ece55b79/UntitledProject/icomoon.woff?xn63nf') format('woff'),
+    url('https://i.icomoon.io/public/temp/51ece55b79/UntitledProject/icomoon.svg?xn63nf#icomoon') format('svg');
+  font-weight: normal;
+  font-style: normal;
+  font-display: block;
+}
+
+[class^="icon-"], [class*=" icon-"] {
+  /* use !important to prevent issues with browser extensions that change fonts */
+  font-family: 'icomoon' !important;
+  speak: never;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+
+  /* Better Font Rendering =========== */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.icon-note-add:before {
+  content: "\e902";
+}
+.icon-note-add1:before {
+  content: "\e903";
+}
+.icon-folder-add:before {
+  content: "\e904";
+}
+.icon-folder-add1:before {
+  content: "\e905";
+}
+.icon-folder-open-o:before {
+  content: "\e900";
+}
+.icon-folder-o:before {
+  content: "\e901";
+}
+.icon-home:before {
+  content: "\e906";
+}
+.icon-bin:before {
+  content: "\e9ac";
+}
+.icon-arrow-left2:before {
+  content: "\ea40";
+}
+</style>
---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,343 @@
+<script>
+export default {
+  data() {
+    return {
+      hover: 999,
+      rootFolders: [],
+      displayFolder: [],
+      displayList: [],
+      routing: [],
+      inHomePage: true,
+      topTitle: 'Bookmarks Bar',
+      folderModal: false,
+      linkModal: false,
+      inputFolderName: '',
+      inputLinkTitle: '',
+      inputLinkUrl: ''
+    };
+  },
+  created () {
+    this.getData();
+  },
+  methods: {
+    // first data
+    getData () {
+      var gettingTree = browser.bookmarks.getTree();
+      this.routing.push("1");
+      gettingTree.then(this.logTree, this.onRejected);
+    },
+    logTree(bookmarkItems) {
+      this.rootFolders = bookmarkItems[0].children;
+      // when initialize , display first tab of top menu
+      this.displayFolder = this.rootFolders[0].children.filter(x => x.url === undefined);
+      this.displayList = this.rootFolders[0].children.filter(x => x.url !== undefined);
+    },
+    clickTopBar(id, title) {
+      this.topTitle = title;
+      this.routing = [];
+      this.routing.push(id);
+      this.getDataById(id);
+    },
+    openFolder(id) {
+      this.routing.push(id);
+      this.getDataById(id);
+      this.inHomePage = false;
+    },
+    goBack() {
+      if (this.routing.length > 1) {
+        let selfId = this.routing.pop();
+        let parentId = this.routing[this.routing.length - 1];
+        if (this.routing.length === 1){
+          this.inHomePage = true;
+        }
+        this.getDataById(parentId);
+      }
+    },
+    getDataById(id) {
+      var gettingBookmarks = browser.bookmarks.getChildren(id);
+      this.folderId = id;
+      gettingBookmarks.then(this.onFulfilled, this.onRejected);
+    },
+    onFulfilled(bookmarks) {
+      this.displayFolder = bookmarks.filter(x => x.url === undefined);
+      this.displayList = bookmarks.filter(x => x.url !== undefined);
+    },
+    onRejected(error) {
+      console.log(`An error: ${error}`);
+    },
+    // getting browser bookmarks data
+    openLink(url) {
+      window.open(url);
+    },
+    openFolderCreation () {
+      this.folderModal = true;
+    },
+    openLinkCreation () {
+      this.linkModal = true;
+    },
+    createFolder (name) {
+      browser.bookmarks.create({ parentId: this.routing[this.routing.length - 1], title: name });
+      this.folderModal = false;
+      this.getDataById(this.routing[this.routing.length - 1]);
+    },
+    createLink(name, url) {
+      var createBookmark = browser.bookmarks.create({
+        title: this.inputLinkTitle,
+        url: this.inputLinkUrl,
+        parentId: this.routing[this.routing - 1]
+      });
+      createBookmark.then(this.getDataById(this.routing[this.routing.length - 1]));
+      this.linkModal = false;
+    },
+    onCreated(node) {
+      console.log(node);
+    },
+    deleteLink (id, parentId) {
+      var removingBookmark = browser.bookmarks.remove(id);
+      removingBookmark.then(this.getDataById(parentId));
+    },
+    deleteFolder (id, parentId) {
+      event.stopPropagation(); 
+      var removingBookmark = browser.bookmarks.remove(id);
+      removingBookmark.then(this.getDataById(parentId));
+    },
+    cancel() {
+      this.folderModal = false;
+    },
+    cancelLink() {
+      this.linkModal = false;
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="main">
+    <div class="header">
+      <div class="header-widget" 
+        @click="clickTopBar(array.id, array.title)"
+        v-for="array in rootFolders" 
+        :key="array.title"
+        :style="{ borderBottom: array.title === topTitle ? '4px solid #000' : 'none'}"
+      >
+        {{ array.title }}
+      </div>
+    </div>
+
+    <div class="body">
+      <div class="body-left">
+        <div class="body-left-folders">
+          <div class="subMenu" 
+            @click="openFolder(folder.id)" 
+            v-for="(folder, index) in displayFolder" 
+            :key="folder.title"
+            @mouseover="hover = index"
+            @mouseleave="hover = 999"
+          >
+            <i class="icon" :class="{'icon-folder-open-o': hover === index, 'icon-folder-o': hover !== index}"/>
+            <div class="subMenu-title">{{ folder.title }}</div>
+            <i class="icon" :class="{'icon-bin': hover === index}" @click="deleteFolder(folder.id, folder.parentId)"/>
+          </div>
+        </div>
+        <div class="body-list">
+          <div class="subList" v-for="list in displayList" :key="list.title">
+            <div class="subList-text" @click="openLink(list.url)">{{ list.title }}</div>
+            <i class="icon icon-bin" @click="deleteLink(list.id, list.parentId)"/>
+          </div>
+        </div>
+      </div>
+      <div class="body-right">
+        <div class="icons">
+          <i class="icon icon-home" v-if="inHomePage" />
+          <i class="icon icon-arrow-left2" v-if="!inHomePage" @click="goBack()"/>
+        </div>
+        <div class="icons">
+          <i class="icon icon-folder-add" @click="openFolderCreation()"/>
+        </div>
+        <div class="icons">
+          <i class="icon icon-note-add" @click="openLinkCreation()"/>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal-mask" v-if="folderModal">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <input v-model="inputFolderName" placeholder="Folder Name">
+          <button @click="createFolder(inputFolderName)">create</button>
+          <button @click="cancel()">Cancel</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal-mask" v-if="linkModal">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div>
+            <input v-model="inputLinkTitle" placeholder="TITLE NAME">
+            <input v-model="inputLinkUrl" placeholder="URL">
+          </div>
+          <button @click="createLink(inputLinkTitle, inputLinkUrl)">create</button>
+          <button @click="cancelLink()">cancel</button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+.main {
+  width: 320px;
+  height: 550px;
+  margin: 0;
+  .header {
+    height: 50px;
+    width: 100%;
+    background-color: #8aacc8;
+    color: black;
+    display: flex;
+    flex-direction: row;
+    &-widget {
+      width: 33%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+      cursor: pointer;
+    }
+  }
+}
+.body {
+  height: 500px;
+  overflow: auto;
+  font-size: 14px;
+  display: flex;
+  flex-direction: row;
+  &-left {
+    width: 450px;
+    height: 100%;
+    overflow: auto;
+    &-folders {
+      display: flex;
+      flex-wrap: wrap;
+      .subMenu {
+        background-color: #f3f6f9;
+        color: #0d4d90;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 50px;
+        width: 95px;
+        margin: 10px;
+        border-radius: 5px;
+        padding: 0 10px;
+        cursor: pointer;
+        position: relative;
+        &-title {
+          width: 80px;
+          text-align: center;
+        }
+        .icon-bin {
+          position: absolute;
+          right: 5px;
+          top: 5px;
+        }
+      }
+      .subMenu:hover {
+        background-color: #0d4d90;
+        color: white;
+      }
+    }
+  }
+  &-left::-webkit-scrollbar {
+    display: none;
+  }
+  &-right {
+    width: 80px;
+    background-color: #0d4d90;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icons {
+      color: white;
+      border-radius: 50%;
+      width: 35px;
+      height: 35px;
+      align-items: center;
+      justify-content: center;
+      display: flex;
+      margin: 10px 0;
+      cursor: pointer;
+      .icon {
+        font-size: 25px;
+      }
+    }
+    .icons:hover {
+      background-color: white;
+      color: #0d4d90;
+    }
+  }
+  
+  &-list {
+    margin: 0 10px;
+    .subList {
+      padding: 8px 0;
+      line-height: 20px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid black;
+      &-text {
+        cursor: pointer;
+      }
+      .icon-bin {
+        color: #0d4d90;
+      }
+    }
+    .subList:hover {
+      background-color: #f3f6f9;
+      color:#0d4d90;
+    }
+  }
+}
+.body::-webkit-scrollbar {
+  display: none;
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+.modal-body {
+  margin: 20px 0;
+}
+
+</style>
